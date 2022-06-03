9929d089e56c60666cb37aed8b14bb88674e570c
dilrong <dilrong@dilrong.com>
2022-06-03 17:03:02 +0900

    feat: logger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from "express"
 import dotenv from "dotenv"
 import FaucetController from "../controllers/faucet";
+import { logger } from "../config/winston";
 
 dotenv.config();
 
 const router = express.Router()
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
+    logger.info("GET /")
     res.status(200).json({
         message: "success",
         version: process.env.npm_package_version,
--- src/services/faucet.ts
+++ src/services/faucet.ts
@@ -1,6 +1,7 @@
 import { Service } from "typedi";
 import axios from "axios";
 import env from "../config";
+import { logger } from "../config/winston";
 
 @Service()
 export default class FaucetService {
@@ -9,11 +10,14 @@ export default class FaucetService {
             const data = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${env.google.recapcha}&response=${token}`);
 
             if (data.data.success) {
+                logger.info("success recaptcha");
                 return { code: 200, message: "success", data: data.data.success }
             } else {
+                logger.info(`error ${data.data["error-codes"]}`);
                 return { code: 400, message: "error", data: data.data["error-codes"] }
             }
         } catch (err) {
+            logger.error(err);
             return { code: 400, message: "error", data: err }
         }
     }
